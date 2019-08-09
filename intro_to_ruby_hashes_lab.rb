d5315d671b7c47f497c2b9f3bf68b9108c9c4aef
K Hong <albtrossfacefall@gmail.com>
2019-08-09 10:21:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,12 @@ def monopoly_with_third_tier
   # complexity.
 {:railroads => {
     :pieces => 4,
-    :rent_in_dollars => 4
+    :rent_in_dollars => {
+      :key1 => 1,
+      :key2 => 2,
+      :key3 => 3,
+      :key4 => 4
+    }
   }
 }
 end
